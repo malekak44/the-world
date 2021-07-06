d408a14f98b434dc7bf2066deb1b66c88de78de3
malekak44 <malekak44@gmail.com>
2021-07-07 01:55:45 +0600

    size fixing

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -7,7 +7,7 @@ const Country = (props) => {
         <div className="card">
             <img src={flag} alt={name} style={{ width: "100%" }} />
             <div className="container">
-                <h2>{name}</h2>
+                <h2>{name.substring(0, 18)}</h2>
                 <p>Capital: {capital}</p>
                 <p>Population: {population}</p>
                 <button onClick={() => props.handleAddClick(props.country)}>Add</button>
